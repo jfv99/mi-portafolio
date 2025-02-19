a1304a30383fa53268b42d0e6cd1f3392584adcd
Juan Felipe Venegas <67348729+jfv99@users.noreply.github.com>
2025-02-18 20:20:52 -0500

    Mejora de diseño x2

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
     console.log("Portafolio cargado correctamente.");
+    
+    let projects = document.querySelectorAll(".project-card");
+    projects.forEach(card => {
+        card.addEventListener("mouseenter", () => {
+            card.style.boxShadow = "5px 5px 15px rgba(255, 255, 255, 0.2)";
+        });
+        card.addEventListener("mouseleave", () => {
+            card.style.boxShadow = "2px 2px 10px rgba(0, 0, 0, 0.1)";
+        });
+    });
 });
